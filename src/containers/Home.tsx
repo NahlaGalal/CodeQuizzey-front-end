98193bde4df39091ac3b62e4ca6bf62084ad7df8
Nahla Galal <nahlaglal@gmail.com>
2020-10-21 04:09:00 +0200

    Add Home page

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,81 +1,125 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import IllustratedImage from "../images/undraw_Choose_bwbs.svg";
+
+interface ICircle {
+  name: string;
+  _id: string;
+}
 
 const Home: React.FC = () => {
+  const [circles, setCircles] = useState<ICircle[]>([]);
+  const [circlesChecked, setCirclesChecked] = useState<string[]>([]);
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+  const [serverErrors, setServerErrors] = useState<any>({});
+
+  useEffect(() => {
+    (async () => {
+      let res = await axios.get("http://localhost:4000/");
+      // let res = await axios.get("http://192.168.1.5:4000/");
+      setCircles(res.data);
+    })();
+  }, []);
+
+  const checkCircle = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+    let circle: string = e.currentTarget.id;
+    setServerErrors({});
+    if (e.currentTarget.checked) {
+      if (circlesChecked.length === 2) e.preventDefault();
+      else setCirclesChecked([...circlesChecked, circle]);
+    } else {
+      let circlesCheckedCopy = [...circlesChecked];
+      let circleIndex = circlesCheckedCopy.findIndex((c) => c === circle);
+      circlesCheckedCopy.splice(circleIndex, 1);
+      setCirclesChecked([...circlesCheckedCopy]);
+    }
+  };
+
+  const submitBasicData = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(true);
+    if (name && email && circlesChecked.length) {
+      (async () => {
+        const inputs = {
+          name,
+          email,
+          circles: [...circlesChecked],
+        };
+        let res = await axios.post("http://localhost:4000/", inputs);
+        let { data } = res;
+        if (data.isFailed) setServerErrors(data.error);
+      })();
+    }
+  };
+
   return (
-    <form className="Home">
-      <div className="field-input">
-        <label htmlFor="name">Your name</label>
-        <input type="text" name="name" id="name" />
-      </div>
-      <div className="field-input">
-        <label htmlFor="email">Your email</label>
-        <input type="email" name="email" id="email" />
-      </div>
-      <div className="field-input">
-        <label>Technical circles</label>
-        <span>You can choose one or two circles only</span>
-        <div className="checkbox-input">
-          <label htmlFor="front">
-            <span></span>Front-end
-          </label>
-          <input type="checkbox" name="circle" id="front" />
+    <main className="Home">
+      <img src={IllustratedImage} alt="Illustrated svg" />
+      <form onSubmit={(e) => submitBasicData(e)}>
+        <div className="text-input">
+          <label htmlFor="name">Your name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => {
+              setName(e.currentTarget.value);
+              setServerErrors({});
+            }}
+          />
+          {error && (!name || serverErrors.name) && (
+            <p className="error">
+              *{serverErrors.name || "You must type your name"}
+            </p>
+          )}
         </div>
-        <div className="checkbox-input">
-          <label htmlFor="back">
-            <span></span>Back-end
-          </label>
-          <input type="checkbox" name="circle" id="back" />
+        <div className="text-input">
+          <label htmlFor="email">Your email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => {
+              setEmail(e.currentTarget.value);
+              setServerErrors({});
+            }}
+          />
+          {error && (!email || serverErrors.email) && (
+            <p className="error">
+              *{serverErrors.email || "You must type your email"}
+            </p>
+          )}
         </div>
-        <div className="checkbox-input">
-          <label htmlFor="data">
-            <span></span>Data Science
-          </label>
-          <input type="checkbox" name="circle" id="data" />
+        <div className="checkbox-group">
+          <label>Technical circles</label>
+          <span>You can choose one or two circles only</span>
+          {circles.map((circle: ICircle) => (
+            <div className="checkbox-group__input" key={circle._id}>
+              <input
+                type="checkbox"
+                name="circle"
+                id={circle._id}
+                onClick={(e) => checkCircle(e)}
+              />
+              <label htmlFor={circle._id}>
+                <span></span>
+                {circle.name}
+              </label>
+            </div>
+          ))}
+          {error && (!circlesChecked.length || serverErrors.circle) && (
+            <p className="error">
+              *{serverErrors.circle || "You must choose one or two circles"}
+            </p>
+          )}
         </div>
-        <div className="checkbox-input">
-          <label htmlFor="security">
-            <span></span>Security
-          </label>
-          <input type="checkbox" name="circle" id="security" />
-        </div>
-        <div className="checkbox-input">
-          <label htmlFor="front">
-            <span></span>Front-end
-          </label>
-          <input type="checkbox" name="circle" id="front" />
-        </div>
-        <div className="checkbox-input">
-          <label htmlFor="graphic">
-            <span></span>Graphic
-          </label>
-          <input type="checkbox" name="circle" id="graphic" />
-        </div>
-        <div className="checkbox-input">
-          <label htmlFor="android">
-            <span></span>Android
-          </label>
-          <input type="checkbox" name="circle" id="android" />
-        </div>
-        <div className="checkbox-input">
-          <label htmlFor="ios">
-            <span></span>IOS
-          </label>
-          <input type="checkbox" name="circle" id="ios" />
-        </div>
-        <div className="checkbox-input">
-          <label htmlFor="embedded">
-            <span></span>Embedded Systems
-          </label>
-          <input type="checkbox" name="circle" id="embedded" />
-        </div>
-        <div className="checkbox-input">
-          <label htmlFor="game">
-            <span></span>Game development
-          </label>
-          <input type="checkbox" name="circle" id="game" />
-        </div>
-      </div>
-    </form>
+        <button className="btn btn__outline" type="submit">
+          Next
+        </button>
+      </form>
+    </main>
   );
 };
 
