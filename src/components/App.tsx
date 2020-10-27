890e2e18b77ede7c00c6f3054592ea31d6423652
Nahla Galal <nahlaglal@gmail.com>
2020-10-28 01:13:31 +0200

    Add Admin authentication

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Admin from '../containers/Admin';
+import AdminAuth from '../containers/AdminAuth';
 import Error from '../containers/Error';
 import Home from '../containers/Home';
 import Question from '../containers/Question';
@@ -10,6 +12,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/question" component={Question} />
+        <Route exact path="/auth" component={AdminAuth} />
+        <Route exact path="/admin" component={Admin} />
         <Route exact path="/error" component={Error} />
         <Redirect to="/error" />
       </Switch>
---
+++ src/containers/AdminAuth.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useRef, useState } from "react";
+import Navbar from "../components/Navbar";
+import IllustratedImage from "../images/undraw_Choose_bwbs.svg";
+import axios from "../axiosInstance";
+import { useCookies } from "react-cookie";
+
+const AdminAuth: React.FC<any> = ({ history }) => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+  const [serverErrors, setServerErrors] = useState<any>({});
+  const [cookies, setCookies] = useCookies(["name"]);
+
+  useEffect(() => {
+    if (cookies.token) history.push("/admin");
+  }, [cookies.token, history]);
+
+  const submitBasicData = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(true);
+    if (email && password) {
+      (async () => {
+        const inputs = {
+          name: "Nahla Galal",
+          email,
+          password,
+        };
+        let res = await axios.post("/auth", inputs);
+        let { data } = res;
+        if (data.isFailed) setServerErrors(data.errors);
+        else {
+          setCookies("token", data.data.token);
+        }
+      })();
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Navbar />
+      <main className="Home">
+        <img src={IllustratedImage} alt="Illustrated svg" />
+        <form onSubmit={(e) => submitBasicData(e)}>
+          <div className="text-input">
+            <label htmlFor="email">Your email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={(e) => {
+                setEmail(e.currentTarget.value);
+                setServerErrors({});
+              }}
+            />
+            {error && (!email || serverErrors?.email) && (
+              <p className="error">
+                *{serverErrors.email || "You must type your email"}
+              </p>
+            )}
+          </div>
+          <div className="text-input">
+            <label htmlFor="password">Your password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={(e) => {
+                setPassword(e.currentTarget.value);
+                setServerErrors({});
+              }}
+            />
+            {error && (!password || serverErrors.password) && (
+              <p className="error">
+                *{serverErrors.password || "You must type your email"}
+              </p>
+            )}
+          </div>
+          <button className="btn btn__outline" type="submit">
+            Login
+          </button>
+        </form>
+      </main>
+    </React.Fragment>
+  );
+};
+
+export default AdminAuth;
