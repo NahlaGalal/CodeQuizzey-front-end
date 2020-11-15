7e03b4bbb70a322769305c9ca664d158b75678c1
Nahla Galal <nahlaglal@gmail.com>
2020-11-15 05:09:38 +0200

    Change backend link to server

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4000/",
+  // baseURL: "http://localhost:4000/",
+  baseURL: "https://codequizzey-back-end.herokuapp.com/",
   //"http://192.168.1.5:4000/"
 });
