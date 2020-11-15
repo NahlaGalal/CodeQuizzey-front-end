fa5aa354e46f902cc464fd2912c51141996d8c8c
Nahla Galal <nahlaglal@gmail.com>
2020-11-15 05:17:12 +0200

    Add server and build to deploy to heroku

---
+++ build/precache-manifest.a8a61ce14ef9a00221731d58e7f33595.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79adecb50f9f0e17427d5c1b2c63077",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5196410f782e9bf8bb4e",
+    "url": "/static/css/2.fa822981.chunk.css"
+  },
+  {
+    "revision": "1b8f734ff80167517dc2",
+    "url": "/static/css/main.511296e6.chunk.css"
+  },
+  {
+    "revision": "5196410f782e9bf8bb4e",
+    "url": "/static/js/2.daf943c7.chunk.js"
+  },
+  {
+    "revision": "5b248e79506059c7ca54a18920a632ca",
+    "url": "/static/js/2.daf943c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b8f734ff80167517dc2",
+    "url": "/static/js/main.91bd78f7.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "dffd1539d6fe91a9545284e1e1e15810",
+    "url": "/static/media/add-circle.dffd1539.svg"
+  },
+  {
+    "revision": "40bbdf6823bb47cbe22907fe45a3f47c",
+    "url": "/static/media/add-question.40bbdf68.svg"
+  },
+  {
+    "revision": "38bd7039f350f361ceb008320da830e0",
+    "url": "/static/media/delete.38bd7039.svg"
+  },
+  {
+    "revision": "ac3265e1fb16ce5246d86398531cea29",
+    "url": "/static/media/download.ac3265e1.svg"
+  },
+  {
+    "revision": "c95d4b10743028ae42d2e0e58bf5be2d",
+    "url": "/static/media/edit.c95d4b10.svg"
+  },
+  {
+    "revision": "09eceecd11f98ba72487036c67d95963",
+    "url": "/static/media/error-401.09eceecd.svg"
+  },
+  {
+    "revision": "19ae4d3f97cdfd09aebda4e43cf6c409",
+    "url": "/static/media/error-500.19ae4d3f.svg"
+  },
+  {
+    "revision": "626d67d5e6a202468dbffdd6ff221bc7",
+    "url": "/static/media/error.626d67d5.svg"
+  },
+  {
+    "revision": "ad687f5a0a08623b495625bd659edf58",
+    "url": "/static/media/fe_arrow-left.ad687f5a.svg"
+  },
+  {
+    "revision": "1d3da98a89ef2959572bba86b10f64e2",
+    "url": "/static/media/fe_arrow-right.1d3da98a.svg"
+  },
+  {
+    "revision": "400b399df3d600df299b6a35e23c901d",
+    "url": "/static/media/no-quiz.400b399d.svg"
+  },
+  {
+    "revision": "c5bdb277a9504d0a4a5da57cc7187004",
+    "url": "/static/media/standing.c5bdb277.svg"
+  },
+  {
+    "revision": "a4bab584dea224963faed3064194b15f",
+    "url": "/static/media/three-dots.a4bab584.svg"
+  },
+  {
+    "revision": "3408a1cf2828723b3ec653351ff8d107",
+    "url": "/static/media/undraw_Choose_bwbs.3408a1cf.svg"
+  }
+]);
